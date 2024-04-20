5c5fc449f5d66d33be2ce9c5dd17f0dd2d0cbd4e
nimanthapriyasad <nimanthapriyasad85@gmail.com>
2024-04-21 00:01:04 +0530

    Change

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -41,7 +41,7 @@ export default function Header() {
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                of type and scrambled it to make a type specimen book dnssvsvk.
               </Typography>
             </Grid>
           </Grid>
